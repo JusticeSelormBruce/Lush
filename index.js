b1551be4d043b666e3efc2f98af8ff741f47f916
justiceselormbruce <justiceselormbruce@gmail.com>
2023-01-11 11:26:26 +0000

    impleted basic api

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
+/**
+ * Base file for  app
+ */
 
+// Dependencies
+var server = require("./lib/server");
+
+// app container
+let app = {};
+
+//app init script
+app.init = function () {
+  server.init();
+};
+
+//call init script
+
+app.init();
